0c22d987bb0d1a85f5d8e6cc9c0bb46763895f10
chhhris <mr.lake@gmail.com>
2013-07-08 18:02:14 -0400

    add checkbox to items form

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ActiveRecord::Base
-  attr_accessible :name, :list_id
-
+  attr_accessible :name, :list_id, :complete
   belongs_to :list
+
+  after_initialize :default_values
+
+  private
+    def default_values
+      self.complete ||= false
+    end 
+
 end
